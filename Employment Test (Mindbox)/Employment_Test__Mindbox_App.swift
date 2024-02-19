afae6d6bb45903df735ade2f5c3214961a01560b
Kayllen <56073470+KayllenZzZ@users.noreply.github.com>
2024-02-19 21:47:49 +0300

    Initial Commit

---
+++ Employment Test (Mindbox)/Employment_Test__Mindbox_App.swift
@@ -0,0 +1,17 @@
+//
+//  Employment_Test__Mindbox_App.swift
+//  Employment Test (Mindbox)
+//
+//  Created by Андрей Чередник on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Employment_Test__Mindbox_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
